d963636c9d28198cdea8f2198b6e9f2e1dbb723a
greiko <nicolas.papanicolaou20@gmail.com>
2022-04-10 09:03:12 -0400

    build: add basic project springboot

---
+++ src/test/java/com/greiko/pipelinekata/PipelineKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greiko.pipelinekata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
